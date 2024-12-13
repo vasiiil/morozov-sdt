152f9e90fedd8f418f8671b96183f237be7bc24a
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2024-12-13 22:26:55 +0300

    Перенес в entity

--- src/pages/orders/model/store.ts
+++
@@ -1,9 +0,0 @@
-import { useReactiveIdentifiedArray } from '@/shared/lib/use/base/useReactiveIdentifiedArray';
-import type { IOrderListItem } from './types';
-
-const orders = useReactiveIdentifiedArray<IOrderListItem>();
-export function useStore() {
-	return {
-		orders,
-	};
-}
