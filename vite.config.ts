125a6015c0c093744c0433b190f46d0abdea0944
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2024-12-13 22:32:57 +0300

    Урл апи + прокси для корсов

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue(), vueDevTools()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [vue(), vueDevTools()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
 		},
-	},
+		server: {
+			proxy: {
+				'/api': {
+					target: env.VITE_API_REAL_URL,
+					changeOrigin: true,
+					secure: false,
+					rewrite: (p) => p.replace(/^\/api/, ''),
+				},
+			},
+			cors: false,
+		},
+	};
 });
