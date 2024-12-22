a423376504d78230ee83cdb9add6ddc8f2015faf
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2024-12-22 13:59:41 +0300

    Вынес тип доставки в entities

---
+++ src/entities/delivery-types/index.ts
@@ -0,0 +1,2 @@
+export { items as deliveryTypeItems } from './model'
+export type { types as DeliveryTypeTypes } from './model';
---
+++ src/entities/delivery-types/model/constants.ts
@@ -0,0 +1,16 @@
+import type { IListItem } from './types';
+export const items: IListItem[] = [
+	{ id: 23, name: 'БелПочта' },
+	{ id: 22, name: 'Почта РФ Курьер Онлайн' },
+	{ id: 21, name: 'Почта РФ Посылка Онлайн' },
+	{ id: 20, name: 'Легкий вес' },
+	{ id: 7, name: 'EMS' },
+	{ id: 6, name: 'Ценная бандероль' },
+	{ id: 5, name: 'Посылка 1 класса' },
+	{ id: 4, name: 'Курьерская служба' },
+	{ id: 3, name: 'Почтой РФ' },
+	{ id: 2, name: 'Самовывоз' },
+	{ id: 1, name: 'Курьерская служба' },
+	{ id: 0, name: 'B2B' },
+	{ id: -1, name: 'Не указан' },
+];
