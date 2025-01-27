eb0727c2b8eede38fcd75e5cdc4d7a5c5e386162
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-01-27 22:34:58 +0300

    Компонент базовой кнопки

---
+++ src/shared/ui/BaseButton.vue
@@ -0,0 +1,45 @@
+<template>
+	<dx-button
+		:element-attr="elementAttr"
+		v-bind="props"
+	/>
+</template>
+
+<script setup lang="ts">
+import { DxButton } from 'devextreme-vue';
+import type { DxButtonTypes } from 'devextreme-vue/cjs/button';
+interface IProps extends /** @vue-ignore */ DxButtonTypes.Properties {
+	iconCircleBorder?: boolean;
+}
+const {
+	iconCircleBorder,
+	elementAttr: _elementAttr,
+	...props
+} = defineProps<IProps>();
+const classes = ['sdt-button'];
+if (_elementAttr?.class) {
+	classes.push(_elementAttr.class);
+}
+if (iconCircleBorder) {
+	classes.push('icon-circle-border');
+}
+const elementAttr = {
+	...(_elementAttr ?? {}),
+	class: classes.join(' '),
+};
+</script>
+
+<style lang="scss" scoped>
+.sdt-button {
+	color: var(--color-text);
+	border-color: var(--color-text);
+	border-radius: 3px;
+	&.icon-circle-border {
+		::v-deep(.dx-icon) {
+			font-size: 14px;
+			border: 1px solid var(--color-text);
+			border-radius: 50%;
+		}
+	}
+}
+</style>
