2e260b2ee228414fc51bb0c7b6c02e7d5b93d942
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-02-09 16:36:50 +0300

    Добавил пустой лэйаут

--- src/app/App.vue
+++ src/app/App.vue
@@ -7,22 +7,38 @@
 		:hide-on-outside-click="false"
 		shading-color="rgba(0,0,0,0.4)"
 	></dx-load-panel>
-  <default-layout>
-    <router-view />
-  </default-layout>
+	<component v-if="routerReady" :is="layout">
+		<router-view />
+	</component>
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { computed, ref, onMounted } from 'vue';
+import { useRoute, useRouter, RouterView } from 'vue-router';
 import { DxLoadPanel } from 'devextreme-vue/load-panel';
 import { locale as dxLocale, loadMessages } from 'devextreme/localization';
 import ruMessages from 'devextreme/localization/messages/ru.json';
 import DefaultLayout from './layouts/DefaultLayout.vue';
+import SimpleLayout from './layouts/SimpleLayout.vue';
 import { useLoader } from '@/shared/lib/use/useLoader';
 
 dxLocale('ru');
 loadMessages(ruMessages);
 const { isLoading } = useLoader();
+const router = useRouter();
+const route = useRoute();
+const routerReady = ref<boolean>(false);
+const layout = computed(() => {
+	if (route.meta.layout === 'simple') {
+		return SimpleLayout;
+	}
+
+	return DefaultLayout;
+});
+onMounted(async () => {
+	await router.isReady();
+	routerReady.value = true;
+});
 </script>
 
 <style scoped></style>
