fd7d4a0d63d23daa175e0eb7a1e131056d292b6b
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-02-09 17:00:29 +0300

    Кнопка сохранить на форме аномалий

--- src/entities/anomaly-zones/ui/PopupCard.vue
+++ src/entities/anomaly-zones/ui/PopupCard.vue
@@ -5,6 +5,15 @@
 		width="50vw"
 		height="70vh"
 	>
+		<dx-toolbar-item
+			toolbar="bottom"
+			location="after"
+		>
+			<base-button
+				text="Сохранить"
+				styling-mode="outlined"
+			></base-button>
+		</dx-toolbar-item>
 		<dx-form
 			v-model:form-data="form"
 			:read-only="true"
@@ -62,7 +71,7 @@
 <script setup lang="ts">
 import { ref, useTemplateRef } from 'vue';
 import type { ComponentExposed } from 'vue-component-type-helpers';
-import { DxPopup } from 'devextreme-vue/popup';
+import { DxPopup, DxToolbarItem } from 'devextreme-vue/popup';
 import {
 	DxForm,
 	DxLabel,
@@ -71,12 +80,13 @@ import {
 	DxRequiredRule,
 } from 'devextreme-vue/form';
 
+import { BaseButton } from '@/shared/ui';
 import { useLoader } from '@/shared/lib/use/useLoader';
 import { useReactiveArray } from '@/shared/lib/use/base/useReactiveArray';
-import { useApi } from '../api';
 import CardProductsDataGrid from './CardProductsDataGrid.vue';
-import type { IItem, ICardProductListItem, IListItem } from '../config';
 import { anomalyStatusItems } from '@/entities/anomaly-statuses';
+import { useApi } from '../api';
+import type { IItem, ICardProductListItem, IListItem } from '../config';
 
 const { startLoading, stopLoading } = useLoader();
 const api = useApi();
