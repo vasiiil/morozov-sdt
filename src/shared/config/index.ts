0342f8c0f4862707e64c87a8ae8584fc97d6af9c
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-02-14 16:42:12 +0300

    Вынес апи токен в отдельную константу

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,7 +1,11 @@
 export const isDev = getRequiredEnvVar('DEV');
 export const isProd = getRequiredEnvVar('PROD');
 
-export const API_URL = '/api/1056df4ff1b93771e18b562e7edb898a';
+export const API_TOKEN = '1056df4ff1b93771e18b562e7edb898a';
+export const API_URL = `/api/${API_TOKEN}`;
+export const HOST = isDev
+	? getRequiredEnvVar('VITE_API_REAL_URL')
+	: document.location.origin;
 
 function getRequiredEnvVar(key: string) {
 	const envVar = getEnvVar(key);
