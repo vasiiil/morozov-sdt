563f24e2677b68a097640158486d008a958356cd
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-02-20 00:23:45 +0300

    Предзагрузка юзера. Пока что не работает

--- src/app/App.vue
+++ src/app/App.vue
@@ -8,42 +8,69 @@
 		shading-color="rgba(0,0,0,0.4)"
 	></dx-load-panel>
 	<base-confirm ref="confirm"></base-confirm>
-	<component v-if="routerReady" :is="layout">
+	<component
+		v-if="userLoaded"
+		:is="layout"
+	>
 		<router-view></router-view>
 	</component>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, onMounted, useTemplateRef, type Ref } from 'vue';
+import {
+	computed,
+	onBeforeMount,
+	onMounted,
+	ref,
+	useTemplateRef,
+	type Ref,
+} from 'vue';
 import type { ComponentExposed } from 'vue-component-type-helpers';
 import { useRoute, useRouter, RouterView } from 'vue-router';
 import { DxLoadPanel } from 'devextreme-vue/load-panel';
 import { locale as dxLocale, loadMessages } from 'devextreme/localization';
 import ruMessages from 'devextreme/localization/messages/ru.json';
-import DefaultLayout from './layouts/DefaultLayout.vue';
-import SimpleLayout from './layouts/SimpleLayout.vue';
-import { useLoader } from '@/shared/lib/use/useLoader';
+
 import { BaseConfirm } from '@/shared/ui';
 import { setComponent } from '@/shared/lib/utils/confirm';
+import { useLoader } from '@/shared/lib/use/useLoader';
+import { useUser } from '@/entities/user';
+
+import DefaultLayout from './layouts/DefaultLayout.vue';
+import SimpleLayout from './layouts/SimpleLayout.vue';
+import { globalRouter } from './providers';
 
 dxLocale('ru');
 loadMessages(ruMessages);
 const { isLoading } = useLoader();
 const router = useRouter();
 const route = useRoute();
-const routerReady = ref<boolean>(false);
-const confirmRef = useTemplateRef<ComponentExposed<typeof BaseConfirm>>('confirm') as Ref<ComponentExposed<typeof BaseConfirm>>;
+const userLoaded = ref<boolean>(false);
+const confirmRef = useTemplateRef<ComponentExposed<typeof BaseConfirm>>(
+	'confirm',
+) as Ref<ComponentExposed<typeof BaseConfirm>>;
 const layout = computed(() => {
-	if (route.meta.layout === 'simple') {
+	if (!userLoaded.value || route.meta.layout === 'simple') {
 		return SimpleLayout;
 	}
 
 	return DefaultLayout;
 });
+async function loadUser() {
+	const { loadUser } = useUser();
+	await loadUser();
+}
+onBeforeMount(async () => {
+	await router.isReady();
+	globalRouter.router = router;
+	globalRouter.route = route;
+	if (route.name !== 'login') {
+		await loadUser();
+	}
+	userLoaded.value = true;
+});
 onMounted(async () => {
 	setComponent(confirmRef.value);
-	await router.isReady();
-	routerReady.value = true;
 });
 </script>
 
