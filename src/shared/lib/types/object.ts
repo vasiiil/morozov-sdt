a0df1cc9f1033c0c35644d484dcf266388be3dde
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-03-09 22:57:44 +0300

    Тип TPrimitiveRecord для типизации body

--- src/shared/lib/types/object.ts
+++ src/shared/lib/types/object.ts
@@ -8,3 +8,12 @@ export type TObjectKeys<
 export type TObjectValues<
 	T extends Record<string, unknown> | Record<number, unknown>,
 > = T[TObjectKeys<T>];
+
+type TPrimitive = string | null | number | boolean;
+export type TPrimitiveRecord = {
+	[key: string]:
+		| TPrimitive
+		| TPrimitive[]
+		| TPrimitiveRecord
+		| TPrimitiveRecord[];
+};
